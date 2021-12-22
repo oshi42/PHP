5f57b4ee2c1928b806d2683fd5f5473b81d7eb2b
2020-5-03-ale <alexey_maximenko@1.ru>
2021-12-23 01:47:06 +0300

    dates

--- phpiseasy/new_year_remaining.php
+++ phpiseasy/new_year_remaining.php
@@ -1,9 +1,16 @@
 <?php
-new_year_remaining("11.11.2011");
+
+# Написать функцию new_year_remaining($__date), 
+# возвращающую количество дней до нового года от заданной даты $__date в формате '22.11.2017'.
+# 
+# Если ввод неправильный - функция возвращает false.
+
 function new_year_remaining($__date) {
     $dateArr = explode(".", $__date);
     $date    = mktime(0, 0, 0, $dateArr[1], $dateArr[0], $dateArr[2]);
+    if ($date == false)
+        return false;
     $nextY   = $dateArr[2] + 1;
     $date2   = mktime(0, 0, 0, 1, 1, $nextY);
-    return ($date2 - $date)/(60*60*24);    
+    return floatval($date2 - $date)/(60*60*24)-1;    
 }
